d54190eccf3c920ab54357433d394dd7ba996fa2
davidbinstock <binstock.d@gmail.com>
2018-04-30 23:00:31 -0500

    almost done with challenge 2

---
+++ bamazonManager.js
@@ -0,0 +1,162 @@
+var inquirer = require('inquirer');
+var mysql = require("mysql");
+require("console.table");
+//===================================
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "david",
+    password: "coffee",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    mainMenu();
+});
+
+function mainMenu(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do, manager?",
+            choices: [
+                {
+                    name: "View Products for Sale",
+                    value: 1
+                },
+                {
+                    name: "View Low Inventory",
+                    value: 2
+                },
+                {
+                    name: "Add to Inventory",
+                    value: 3
+                },
+                {
+                    name: "Add New Product",
+                    value: 4
+                }
+            ],
+            name: "choice"
+        }
+    ]).then(function(response){
+        switch(response.choice){
+            case 1:
+                displayProducts();
+                break;
+            case 2:
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "Show products with stock quantity less than:",
+                        name: "max"
+                    }
+                ]).then(function(response){
+                    displayLowInventory(response.max);
+                })
+                break;
+            case 3:
+                addToInventory();
+                break;
+            case 4:
+        }
+        
+    })
+}
+
+function displayProducts(){
+    //Display a table of the items
+    var query = connection.query(
+        "SELECT * FROM products",
+        function(err, res) {
+            console.table(res);
+            mainMenu();
+        }
+    );
+}
+
+function displayLowInventory(maxQuantity){
+    var query = connection.query(
+        "SELECT * FROM products WHERE stock_quantity < ?",
+        [maxQuantity],
+        function(err, res) {
+            if(res.length >= 1){
+                console.table(res);
+            }else{
+                console.log("There are no products with a stock quantity less than "+ maxQuantity)
+            }
+            mainMenu();
+        }
+    );
+}
+
+function addToInventory(){
+    //get all products and store names into an array
+    var query = connection.query(
+        "SELECT * FROM products",
+        function(err, res) {
+            var choicesArray = res.map(function(val,ind,arr){
+                var id = val.item_id;
+                var itemName = val.product_name;
+                var quantity = val.stock_quantity;
+                return {
+                    name: itemName+" (current stock: "+ quantity+")",
+                    value: id
+                }
+            })
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "Which product would you like to inventory to?",
+                    choices: choicesArray,
+                    name: "productID"
+                }
+            ]).then(function(response){
+                var id = response.productID
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "How much stock would you like to add to the inventory?:",
+                        name: "add"
+                    }
+                ]).then(function(response){
+                    var addQuantity = response.add;
+                    console.log(id)
+                    var query = connection.query(
+                        "SELECT * FROM products WHERE ?",
+                        [{
+                            item_id: id
+                        }],
+                        function(err, res) {
+                            var exQuantity = res[0].stock_quantity;
+                            console.log("existing stock: ", exQuantity);
+                            var newQuantity = exQuantity += parseInt(addQuantity);
+                            console.log("add stock: ", addQuantity);
+                            console.log("new stock: ", newQuantity);
+                            var query = connection.query(
+                                "UPDATE products SET ? WHERE ?",
+                                [
+                                  {
+                                    stock_quantity: newQuantity
+                                  },
+                                  {
+                                    item_id: id
+                                  }
+                                ],
+                                function(err, res) {
+                                  console.log(res.affectedRows + " products updated!\n");
+                                  mainMenu();
+                                }
+                              );
+                        }
+                    );
+
+                })
+
+            })
+        }
+    );
+}
--- bamazonSeed.sql
+++ bamazonSeed.sql
@@ -1,1 +1,27 @@
-SELECT * FROM bamazon_db.products;
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30),
+    price DECIMAL(10,2),
+    stock_quantity INTEGER(10),   
+    PRIMARY KEY(item_id)
+);
+
+INSERT INTO products
+VALUES
+	(NULL, 'Smelly Socks', 'Dirty Clothes', 8.99, 2000),
+	(NULL, 'Sweaty Shirts', 'Dirty Clothes', 14.87, 1000),
+	(NULL, 'Muddy Pants', 'Dirty Clothes', 21.65, 1500),
+    (NULL, 'Broken Toaster', 'Broken Things', 16.61, 500),
+    (NULL, 'Broken Record Player', 'Broken Things', 51.42, 200),
+    (NULL, 'Broken Chair', 'Broken Things', 13.13, 700),
+    (NULL, 'Thneeds', 'Dr Suess', 120.79, 1750),
+    (NULL, 'Clown Shoes', 'Silly Stuff', 30.45, 200),
+    (NULL, 'Fake Mustache', 'Silly Stuff', 1.96, 200),
+    (NULL, 'Jar of Dirt', 'Misc', 357.23, 200),
+    (NULL, 'Stale Bread', 'Misc', 30.45, 200)
+;
